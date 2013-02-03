f48cdabef4d08fd01a6609b7cc599c0ae4aa6b76
Leif Gensert <leif@gensert.de>
2013-02-03 01:18:04 +0100

    add reducing and addition

--- lib/riese.rb
+++ lib/riese.rb
@@ -8,5 +8,34 @@ module Riese
       @numerator = numerator
       @denominator = denominator
     end
+
+    def == other
+      other && self.numerator == other.numerator && self.denominator == other.denominator
+    end
+
+    def + other
+      to_add = other.nil? ? Fraction.new(0, 1) : other
+      self.class.reduced self.numerator * to_add.denominator + to_add.numerator * self.denominator, self.denominator * to_add.denominator
+    end
+
+    def self.reduced numerator, denominator
+      gcd = self.gcd numerator, denominator
+      if [0, 1].include? numerator
+        Fraction.new(numerator, denominator)
+      else
+        Fraction.new(numerator/gcd, denominator/gcd)
+      end
+    end
+
+    private
+    def self.gcd first, second
+      return first if second == 0
+      return second if first == 0
+      if first > second
+        self.gcd first - second, second
+      else
+        self.gcd first, second - first
+      end
+    end
   end
 end
--- test/test_riese.rb
+++ test/test_riese.rb
@@ -3,9 +3,38 @@ require 'riese'
 require 'turn'
 
 class TestRiese < MiniTest::Unit::TestCase
-  def test_assignment
+  def test_that_assignment_is_stored_correctly
     subject = Riese::Fraction.new 1, 2
     assert_equal 1, subject.numerator
     assert_equal 2, subject.denominator
+  end
+
+  def test_that_2_fractions_with_same_values_are_equal
+    subject = Riese::Fraction.new 1, 2
+    assert_equal Riese::Fraction.new(1, 2), subject
+  end
+
+  def test_that_nil_is_unequal_to_fraction
+    refute_equal Riese::Fraction.new(1, 2), nil
+  end
+
+  def test_that_fraction_is_reduced_correctly_for_simple_fraction
+    assert_equal Riese::Fraction.reduced(2, 4), Riese::Fraction.new(1, 2)
+  end
+
+  def test_that_fraction_is_reduced_correctly_for_fraction_that_is_already_shortened
+    assert_equal Riese::Fraction.reduced(1, 10), Riese::Fraction.new(1, 10)
+  end
+
+  def test_that_fraction_is_reduced_correctly_for_fraction_with_zero_as_numerator
+    assert_equal Riese::Fraction.reduced(0, 100), Riese::Fraction.new(0, 100)
+  end
+
+  def test_that_addition_works_for_simple_fraction
+    assert_equal Riese::Fraction.new(3, 4), Riese::Fraction.new(1, 2) + Riese::Fraction.new(1, 4)
+  end
+
+  def test_that_addition_works_for_addition_with_nil
+    assert_equal Riese::Fraction.new(1, 2), Riese::Fraction.new(1, 2) + nil
   end
 end
